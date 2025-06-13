87b118a021a7fc1cbccd6c2355fcbf0a55755b5d
BillCharalampidis <vasileiosxaral@gmail.com>
2025-06-13 21:15:33 +0300

    Προσθήκη fetch request με AJAX για τον αλλαγή του checkbox στο toggle και προσθήκη class=done στο style

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-// script.js
+const checkboxes = document.querySelectorAll('input[type="checkbox"][data-id]');
+checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', () => {
+        const id = checkbox.getAttribute('data-id');
+        //fetch request με AJAX
+        fetch(`toggle.php?id=${id}`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.success){
+                checkbox.classList.toggle('done', checkbox.checked);
+            } else {
+                alert('Κάτι πήγε στραβά με το toggle.');
+                checkbox.checked = !checkbox.checked;
+            }
+        })
+        .catch(() => {
+            alert('Σφάλμα δικτύου.');
+            checkbox.checked = !checkbox.checked;
+        })
+    })
+});
