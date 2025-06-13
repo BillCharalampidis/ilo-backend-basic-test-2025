1d570141f393134fc4741214c1e6ad77f5a7e7bb
BillCharalampidis <vasileiosxaral@gmail.com>
2025-06-13 18:51:53 +0300

    Υλοποίηση λογικής για προσθήκη νέων tasks

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
-    
+    if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+        $title = trim($_POST['title'] ?? '');
+        $description = trim($POST['description'] ?? '');
+        if ( strlen($title) >=3) {
+            $file = 'tasks.json';
+            $tasks = [];
+            if (file_exists($file)){
+                $json = file_get_contents($file);
+                $tasks = json_decode($json, true);
+            }
+
+            $ids = array_column($tasks, 'id');
+            $newId = $ids ? max(ids) +1 : 1;
+
+            
+        }
+    }
+
 ?>
